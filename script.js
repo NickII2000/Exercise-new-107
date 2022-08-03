44ed4da79a638d3e4dd98f2a5027c96b7c1dd0a1
Nick <Nick_II2000@mail.ru>
2022-08-03 21:15:19 +0600

    2-th commit

--- script.js
+++ script.js
@@ -13,10 +13,16 @@ const data = [
         id: 'circle',
         tag: 'span'
     }
-]
+];
 
 data.forEach(blockObj => {
     const block = document.createElement(blockObj.tag);
+
+    if (!blockObj.id) {
+        throw 'error';
+    }
+
     block.setAttribute('id', blockObj.id);
+    block.textContent = `tag = ${blockObj.tag}, id = ${blockObj.id}`;
     document.body.append(block);
-})
+});
