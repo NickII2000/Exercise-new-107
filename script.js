95ca4f3609002a7aed008c9dc5fc87269b18afd8
Nick <Nick_II2000@mail.ru>
2022-08-03 21:29:05 +0600

    -th commit

--- script.js
+++ script.js
@@ -15,18 +15,36 @@ const data = [
     }
 ];
 
-data.forEach((blockObj, i) => {
-    const block = document.createElement(blockObj.tag);
+// data.forEach((blockObj, i) => {
+//     const block = document.createElement(blockObj.tag);
 
-    if (!blockObj.id) {
-        throw new Error(`В данных под номером ${i + 1} нет id`);
-    }
+//     if (!blockObj.id) {
+//         throw new Error(`В данных под номером ${i + 1} нет id`);
+//     }
 
-    block.setAttribute('id', blockObj.id);
-    block.textContent = `tag = ${blockObj.tag}, id = ${blockObj.id}`;
-    document.body.append(block);
-});
+//     block.setAttribute('id', blockObj.id);
+//     block.textContent = `tag = ${blockObj.tag}, id = ${blockObj.id}`;
+//     document.body.append(block);
+// });
+
+
+try {
+    data.forEach((blockObj, i) => {
+        const block = document.createElement(blockObj.tag);
+
+        if (!blockObj.id) {
+            throw new SyntaxError(`В данных под номером ${i + 1} нет id`);
+        }
+
+        block.setAttribute('id', blockObj.id);
+        block.textContent = `tag = ${blockObj.tag}, id = ${blockObj.id}`;
+        document.body.append(block);
+    });
+} catch (err) {
+    // console.log('\n', err.name, '\n', err.message, '\n', err.stack);
+    console.error('\n', err.name, '\n', err.message, '\n', err.stack);
+}
 
 // const err = new Error('Пример ошибки');
-const err = new SyntaxError('Пример ошибки');
-console.log('\n', err.name, '\n', err.message, '\n', err.stack);
+// const err = new SyntaxError('Пример ошибки');
+// console.log('\n', err.name, '\n', err.message, '\n', err.stack);
