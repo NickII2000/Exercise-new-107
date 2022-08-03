3c989dcd45d2ba65890067cfe95c82880a8c963a
Nick <Nick_II2000@mail.ru>
2022-08-03 21:22:03 +0600

    4-th commit

--- script.js
+++ script.js
@@ -19,10 +19,13 @@ data.forEach((blockObj, i) => {
     const block = document.createElement(blockObj.tag);
 
     if (!blockObj.id) {
-        throw new Error(`В данных под номером ${i + 1} нет id`);
+        // throw new Error(`В данных под номером ${i + 1} нет id`);
     }
 
     block.setAttribute('id', blockObj.id);
     block.textContent = `tag = ${blockObj.tag}, id = ${blockObj.id}`;
     document.body.append(block);
 });
+
+const err = new Error('Пример ошибки');
+console.log('\n', err.name, '\n', err.message, '\n', err.stack);
